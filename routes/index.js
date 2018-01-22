bf95e252273c0e865daef6455933b495242ed200
ivy <unjust@gmail.com>
2018-01-21 20:02:39 -0500

    first templates and rough styles

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Hola Mundo!' });
+});
+
+router.get('/work', function(req, res, next) {
+  res.render('work', { title: 'Work' });
+});
+
+router.get('/play', function(req, res, next) {
+  res.render('play', { title: 'Play' });
 });
 
 module.exports = router;
